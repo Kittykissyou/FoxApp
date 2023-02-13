e585e1fe39ea08c51d5b0fa495fb965fa8529bc4
kittyKissYou <ubiuka@yandex.ru>
2023-02-13 16:59:08 +0900

    PC ver is done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const treangle = document.getElementById('treangle');
 const context = document.getElementById('context');
+const dropdown = document.getElementById('dropdown');
 treangle.onclick = function () {
   context.classList.toggle('dropdown__visible-context');
   this.classList.toggle('dropdown__treangle-rotate');
 };
-const question = ['Вопрос в несколько слов', 'Вопрос в несколько букв'];
